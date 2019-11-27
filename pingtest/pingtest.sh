2f766c0830e64698451f0f746b5269819e2964d5
pwt <1089749+pwt@users.noreply.github.com>
2019-11-27 10:46:22 +0000

    Added pingtest script

---
+++ pingtest/pingtest.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [ $# != 2 ]
+then
+    echo "Usage:" $0 "<Host_or_IP> <Delay_between_pings_(s)>"
+    exit
+fi
+HOST=$1
+SLEEP=$2
+
+STATE="NEW"
+OLDSTATE="NEW"
+echo $(date "+%Y-%m-%d : %H:%M:%S :") "Starting pings to" $HOST", waiting" $SLEEP"s after each ping"
+while true
+do
+    ping -c 1 -W 3 $HOST > /dev/null
+    if [ $? == 0 ]
+    then
+        STATE="UP"
+    else
+        STATE="DOWN"
+    fi
+    if [ $STATE != $OLDSTATE ]
+    then
+        echo $(date "+%Y-%m-%d : %H:%M:%S :") "Host" $HOST "State is" $STATE
+        OLDSTATE=$STATE
+    fi
+    sleep $SLEEP
+done
